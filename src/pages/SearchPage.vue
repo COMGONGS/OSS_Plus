46c738ba6295043149c2470cd2f149fd0a49455a
SpaceDust75 <aacc9058@gmail.com>
2023-06-12 09:13:07 +0900

    상담사 찾기 페이지 상담사 profilecard 임시 생성 profilecard에 입력한 내용은 전부 임의로 설정한 것임 불러와야하는 데이터 : 이미지파일(상담사가 등록한 이미지), 기관 이름(ex. 제주지역상담센터), 주소(ex. 제주시 아라동 xx길), 연락처(064-xxx-xxxx)

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -3,7 +3,29 @@
     <BannerPage title="Search" />
     <div class="maincontents">
       <!--이 부분에 상담사 목록 나열 table 추가 하기-->
-      내용추가
+      <ul class="list">
+        <li class="profile-card">
+          <a>
+            <figure>
+              <img alt="XX 사진" 
+              src="https://ifh.cc/g/pWHtCo.png">
+            </figure>
+            <p class="name">
+
+            </p>
+            <p class="intro">
+
+            </p>
+            <!--dl의 경우 세부 설명을 list 형태로 나열하기 위해 들고옴 그냥 p로 바꿔도 괜춘할듯-->
+            <dl class="location">
+              <dt>제주지역상담센터</dt>
+              <dd>제주시 아라동 xx길</dd>
+              <dd>064-xxx-xxxx</dd>
+            </dl>
+            <p class="detail">자세히</p>
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -30,5 +52,61 @@ export default {
   background-color: gray;
   background-repeat: no-repeat;
   background-size: cover;
+}
+
+.ol, ul, li {
+  list-style: none;
+}
+
+.list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 40px 0 -10px;
+}
+
+.profile-card {
+  background: white;
+  border-radius: var(--Radius-L);
+}
+
+ul.list > li {
+  position: relative;
+  flex: 0 1 290px;
+  margin: 40px 10px 10px;
+  border: 1px solid #fff;
+  box-sizing: border-box;
+  transition: all 0.3s;
+}
+
+.ul.list figure {
+  position: absolute;
+  top: -30px;
+  width: 100px;
+  height: 120px;
+  background-color: #fff;
+  background-size: cover;
+  border: 1px solid var(--Lic-Line);
+  border-radius: var(--Radius-M);
+  overflow: hidden;
+}
+
+.figure {
+  line-height: 0;
+}
+
+ul.list figure img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center 5%;
+}
+
+.img {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  overflow-clip-margin: content-box;
+  overflow: clip;
 }
 </style>
