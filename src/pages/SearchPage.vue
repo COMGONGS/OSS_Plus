fe9b86cdeef8d1c50f4c3fe9f2d9e33ff170bef1
SpaceDust75 <aacc9058@gmail.com>
2023-06-12 18:12:43 +0900

    상담사찾기 카드 섹션 복사(한줄에 5개 최대로 설정) flex 이용하여 세로 정렬 구현 전 커밋

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -24,6 +24,89 @@
             </dl>
             <p class="detail">자세히</p>
           </a>
+        </li><li class="profile-card">
+          <a>
+            <figure>
+              <img alt="XX 사진" 
+              src="https://ifh.cc/g/pWHtCo.png">
+            </figure>
+            <p class="name">
+
+            </p>
+            <p class="intro">
+
+            </p>
+            <!--dl의 경우 세부 설명을 list 형태로 나열하기 위해 들고옴 그냥 p로 바꿔도 괜춘할듯-->
+            <dl class="location">
+              <dt>제주지역상담센터</dt>
+              <dd>제주시 아라동 xx길</dd>
+              <dd>064-xxx-xxxx</dd>
+            </dl>
+            <p class="detail">자세히</p>
+          </a>
+        </li>
+        <li class="profile-card">
+          <a>
+            <figure>
+              <img alt="XX 사진" 
+              src="https://ifh.cc/g/pWHtCo.png">
+            </figure>
+            <p class="name">
+
+            </p>
+            <p class="intro">
+
+            </p>
+            <!--dl의 경우 세부 설명을 list 형태로 나열하기 위해 들고옴 그냥 p로 바꿔도 괜춘할듯-->
+            <dl class="location">
+              <dt>제주지역상담센터</dt>
+              <dd>제주시 아라동 xx길</dd>
+              <dd>064-xxx-xxxx</dd>
+            </dl>
+            <p class="detail">자세히</p>
+          </a>
+        </li>
+        <li class="profile-card">
+          <a>
+            <figure>
+              <img alt="XX 사진" 
+              src="https://ifh.cc/g/pWHtCo.png">
+            </figure>
+            <p class="name">
+
+            </p>
+            <p class="intro">
+
+            </p>
+            <!--dl의 경우 세부 설명을 list 형태로 나열하기 위해 들고옴 그냥 p로 바꿔도 괜춘할듯-->
+            <dl class="location">
+              <dt>제주지역상담센터</dt>
+              <dd>제주시 아라동 xx길</dd>
+              <dd>064-xxx-xxxx</dd>
+            </dl>
+            <p class="detail">자세히</p>
+          </a>
+        </li>
+        <li class="profile-card">
+          <a>
+            <figure>
+              <img alt="XX 사진" 
+              src="https://ifh.cc/g/pWHtCo.png">
+            </figure>
+            <p class="name">
+
+            </p>
+            <p class="intro">
+
+            </p>
+            <!--dl의 경우 세부 설명을 list 형태로 나열하기 위해 들고옴 그냥 p로 바꿔도 괜춘할듯-->
+            <dl class="location">
+              <dt>제주지역상담센터</dt>
+              <dd>제주시 아라동 xx길</dd>
+              <dd>064-xxx-xxxx</dd>
+            </dl>
+            <p class="detail">자세히</p>
+          </a>
         </li>
       </ul>
     </div>
